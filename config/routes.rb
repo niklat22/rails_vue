81bb6cf5e0aebdc675118ad965c3475d2ffb85e7
Nikund <wc.nikund@gmail.com>
2020-02-05 15:29:37 +0530

    Remove some routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  namespace 'api' do
-    namespace 'v1' do
-      resources :projects
-    end
-  end
-
   post '/users/:user_id/projects', to: 'projects#create'
   get '/users/:user_id/projects', to: 'projects#index'
   post '/users/:user_id/projects/:id/add', to: 'projects#add'
